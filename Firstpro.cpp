0ceff7839882ba106e18e3009c341694054cb218
AnkithChowdary <ankithc29@gmail.com>
2023-10-23 00:11:23 +0530

    Update

---
+++ Firstpro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter your favourite number:";
+   cin>>n;
+   cout<<n<<endl;
+   
+   cout<<"i don't like your number"<<endl;
+
+  return 0;
+}
